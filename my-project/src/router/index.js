f618d8f9b8b313f48b09449e6a8ad53eac1d92ff
zzzzz2222 <111111@126.com>
2021-06-12 23:16:32 +0800

    404

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "*",
+    name: "NoFound",
+    component: ()=>import("../views/NoFound.vue"),
   },
 ];
 
